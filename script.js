54cc19effd477b40a92ed1aa8e8ffef7218c7c35
cristinadeviza <deviza.cristina.98@gmail.com>
2024-11-18 19:10:38 +0100

    second commit

--- script.js
+++ script.js
@@ -37,6 +37,12 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
+
+  scorers: {
+    Gnarby: 1,
+    Hummels: 1,
+    Lewandowski: 2,
+  },
 };
 
 //1)
@@ -67,3 +73,57 @@ printGoals(...game.scored);
 
 //7)
 team1 < team2 && console.log('Team 1 is more likely to win');
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+//1
+for (const [index, player] of game.scored.entries()) {
+  console.log(`Gol ${index + 1}: ${player}`);
+}
+
+//2
+sum = 0;
+count = 0;
+const odd = game.odds;
+
+// 1 Method
+// for (const key in odd) {
+//   sum += odd[key];
+//   count++;
+// }
+// const average = count > 0 ? sum / count : 0; //nu impartim la 0
+// console.log(average);
+
+//2 Method
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+//3
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : 'victory';
+  {
+    game[team];
+  }
+  console.log(`Odd of  ${teamStr}: ${odd}`);
+}
